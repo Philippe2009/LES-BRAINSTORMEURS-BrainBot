05f50920fba40daa62d4e60da6ff1571f57a0ec6
Philippe2009 <45831602+Philippe2009@users.noreply.github.com>
2021-10-23 17:40:23 +0200

    Update GyroTurn.py

--- GyroTurn.py
+++ GyroTurn.py
@@ -7,23 +7,49 @@ import math
 # Create your objects here.
 hub = MSHub()
 
+#variable des moteurs
+pair = MotorPair('E','F')
+#tourner à 90 degrés
+#reset le yaw du gyro
+hub.motion_sensor.reset_yaw_angle()
+#variable de l'angle de destination
+"""
+val = 90
+pair.start_tank(8, -8)
+while hub.motion_sensor.get_yaw_angle() <= val:
+    print(str(hub.motion_sensor.get_yaw_angle()))
+    pass
+pair.stop()
+print(str(hub.motion_sensor.get_yaw_angle()))
+"""
 
-# Write your program here.
-hub.speaker.beep()
-# la variable des moteurs
-motors = MotorPair('E','F')
-
-def gyroTurn(val,speed):
-   hub.motion_sensor.reset_yaw_angle()
-   if val > 0:
-       #tourner à droite
-       print("droite")
-       while hub.motion_sensor.get_yaw_angle() >= val:
-           motors.start(100,speed)  
-   else:
-       #tourner à gauche
-       print("gauche")
-
-gyroTurn(11,10)
-
- 
+# la fonction pour tourner avec le gyro
+# elle prend en paramètre la paire de moteurs et l'angle demandé
+# il y environ 2 degrés de marge
+def gyroTurn(motors, angle):
+    #vérifie si la valeur est positiver est négative
+    #divise angle par la valeur absolue de l'angle (si angle = -1 alors valeur absolue = 1 et à l'inverse si angle = 1 alors valeur absolue = -1)
+    #si l'angle est négatif, l'angle = 1 et si ml'angle est positif, angle = -1
+    direction = angle / math.fabs(angle)  
+    print("direction:",direction)
+    motors.start_tank(8*int(direction),-8*int(direction))
+    print("direction:",direction)
+    # example: -1*2 <= -1*-90
+    # donc, si -2 <= 90
+    while direction*hub.motion_sensor.get_yaw_angle() <= direction*angle:
+        print(str(hub.motion_sensor.get_yaw_angle()))
+        pass
+    if angle > 0:
+        # le robot tourne à droite
+    
+        # le robot tourne à gauche
+    else:
+        while hub.motion_sensor.get_yaw_angle() >= angle:
+            print(str(hub.motion_sensor.get_yaw_angle()))
+            pass
+    motors.stop()
+    print(str(hub.motion_sensor.get_yaw_angle()))
+        
+    
+        
+gyroTurn(pair,90)
