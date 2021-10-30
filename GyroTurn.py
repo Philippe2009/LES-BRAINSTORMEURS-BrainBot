11328c4a8b26775a6b40b38b8394bfca98e3b85c
Philippe2009 <45831602+Philippe2009@users.noreply.github.com>
2021-10-30 15:48:16 +0200

    last

--- GyroTurn.py
+++ GyroTurn.py
@@ -26,13 +26,14 @@ print(str(hub.motion_sensor.get_yaw_angle()))
 # la fonction pour tourner avec le gyro
 # elle prend en paramètre la paire de moteurs et l'angle demandé
 # il y environ 2 degrés de marge
-def gyroTurn(motors, angle):
+def gyroTurn(motors, angle,speed = 8):
+    hub.motion_sensor.reset_yaw_angle()
     #vérifie si la valeur est positiver est négative
     #divise angle par la valeur absolue de l'angle (si angle = -1 alors valeur absolue = 1 et à l'inverse si angle = 1 alors valeur absolue = -1)
     #si l'angle est négatif, l'angle = 1 et si ml'angle est positif, angle = -1
-    direction = angle / math.fabs(angle)  
+    direction = angle / math.fabs(angle)
     print("direction:",direction)
-    motors.start_tank(8*int(direction),-8*int(direction))
+    motors.start_tank(speed*int(direction),-speed*int(direction))
     print("direction:",direction)
     # example: -1*2 <= -1*-90
     # donc, si -2 <= 90
@@ -43,6 +44,10 @@ def gyroTurn(motors, angle):
     print(str(hub.motion_sensor.get_yaw_angle()))
 
 #carré
-for i in range(3):
-    pair.move_tank(10, 'cm', 10, 10)
-    gyroTurn(pair,-90)
+def square(speed):
+    pair.set_stop_action('coast')
+    for i in range(4):
+        pair.move_tank(10, 'cm', speed, speed)
+        gyroTurn(pair,-90,10)
+
+#square(10)
