6d348bf7f3369274ab2a0bf6c45e4fc20de13b25
Philippe2009 <45831602+Philippe2009@users.noreply.github.com>
2021-12-04 15:57:56 +0100

    ajout progressiveGyroturn

--- GyroTurn.py
+++ GyroTurn.py
@@ -4,6 +4,7 @@ from mindstorms.operator import greater_than, greater_than_or_equal_to, less_tha
 import math
 
 
+
 # Create your objects here.
 hub = MSHub()
 
@@ -46,6 +47,22 @@ def gyroTurn(motors, angle,speed = 8):
     motors.stop()
     print(str(hub.motion_sensor.get_yaw_angle()))
 
+def progressiveGyroturn(robot, angle):
+    def get_distance(a, b):
+        return int(math.fabs(math.fabs(a) - math.fabs(b)))
+    robot.set_stop_action('brake')
+    hub.motion_sensor.reset_yaw_angle()
+    direction = int(angle / math.fabs(angle)) # 1 is clockwise,-1 is counterclockwise
+    current_angle = 0
+    distance = angle * direction
+    speed = 0
+    while current_angle*direction < angle*direction:
+        print()
+        robot.start_tank(speed* direction, speed*direction*-1)
+        current_angle = hub.motion_sensor.get_yaw_angle()
+        distance = get_distance(current_angle, angle)
+        speed = int(distance / math.fabs(angle) * 25) + 5 # we get a number between 5 and 30
+    robot.stop()
 #carré
 def square(speed,gyroSpeed = 10):
     pair.set_stop_action('coast')
@@ -164,6 +181,3 @@ moveForwardCm(Mpair,10,30)
 gyroTurn(Mpair,90,10)
 """
 platooningTrucks()
-
-
-    
