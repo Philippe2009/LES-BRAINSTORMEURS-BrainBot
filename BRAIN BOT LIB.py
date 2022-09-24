cf42d5df4fcb207a0462ff8810944295a2092dff
Philippe2009 <45831602+Philippe2009@users.noreply.github.com>
2022-09-24 17:14:34 +0200

    Update BRAIN BOT LIB.py

--- BRAIN BOT LIB.py
+++ BRAIN BOT LIB.py
@@ -3,6 +3,7 @@ from mindstorms.control import wait_for_seconds, wait_until, Timer
 from mindstorms.operator import greater_than, greater_than_or_equal_to, less_than, less_than_or_equal_to, equal_to, not_equal_to
 import math
 
+# THE LIBRARY FOR THE BRAIN BOT 2.0
 
 # Create your objects here.
 hub = MSHub()
@@ -14,6 +15,7 @@ hub.speaker.beep()
 brain_bot = MotorPair('B', 'C')
 # moteur de droite
 right_motor = Motor('C')
+# on reset le moteur
 right_motor.set_degrees_counted(0)
 # nous définissons que quand le robot s'arrête, il ne freine pas
 brain_bot.set_stop_action('coast')
@@ -23,15 +25,15 @@ brain_bot.set_stop_action('coast')
 # le paramètre "robot_sensor_degrees" prends la variable du moteur qui va  servir de capteur pour le nombre de degrés parcouru
 # le paramère "degrees" prends le nombre de degrés que l'on va parcourrir
 # le paramètre "speed" pends en compte la vitesse
-def move_until(robot,motor_sensor_degrees, degrees,speed):
-    while motor_sensor_degrees.get_degrees_counted() <= degrees:
+def move_until(robot,motor_sensor_degrees, distance_degrees,speed):
+    while abs(motor_sensor_degrees.get_degrees_counted()) <= abs(degrees):
         robot.start_tank(speed, speed)
         print(motor_sensor_degrees.get_degrees_counted())
     robot.stop()
     motor_sensor_degrees.set_degrees_counted(0)
-# le programme 
-while  right_motor.get_degrees_counted() <= 1000:
-    brain_bot.start_tank(50, 50)
-    print(right_motor.get_degrees_counted())
-brain_bot.stop()
-right_motor.set_degrees_counted(0)
+    if distance_degrees < 0:
+        print("ERREUR: LA VARIABLE DE LA DISTANCE NE DOIT PAS ETRE NEGATIVE(VOIR FONCTION move_until())
+
+# la mission "television"
+move_until(brain_bot,right_motor, 1250, 50) 
+move_until(brain_bot,right_motor, 1250, -100)
