353cd89dc257a533ae98f9e85bc1e3d354aa373e
Philippe2009 <45831602+Philippe2009@users.noreply.github.com>
2022-09-24 17:20:28 +0200

    Update BRAIN BOT LIB.py

--- BRAIN BOT LIB.py
+++ BRAIN BOT LIB.py
@@ -15,7 +15,6 @@ hub.speaker.beep()
 brain_bot = MotorPair('B', 'C')
 # moteur de droite
 right_motor = Motor('C')
-# on reset le moteur
 right_motor.set_degrees_counted(0)
 # nous définissons que quand le robot s'arrête, il ne freine pas
 brain_bot.set_stop_action('coast')
@@ -26,13 +25,13 @@ brain_bot.set_stop_action('coast')
 # le paramère "degrees" prends le nombre de degrés que l'on va parcourrir
 # le paramètre "speed" pends en compte la vitesse
 def move_until(robot,motor_sensor_degrees, distance_degrees,speed):
-    while abs(motor_sensor_degrees.get_degrees_counted()) <= abs(degrees):
+    while abs(motor_sensor_degrees.get_degrees_counted()) <= abs(distance_degrees):
         robot.start_tank(speed, speed)
         print(motor_sensor_degrees.get_degrees_counted())
     robot.stop()
     motor_sensor_degrees.set_degrees_counted(0)
     if distance_degrees < 0:
-        print("ERREUR: LA VARIABLE DE LA DISTANCE NE DOIT PAS ETRE NEGATIVE(VOIR FONCTION move_until())
+        print("ERREUR: LA VARIABLE DE LA DISTANCE NE DOIT PAS ETRE NEGATIVE(VOIR FONCTION move_until())")
 
 # la mission "television"
 move_until(brain_bot,right_motor, 1250, 50) 
