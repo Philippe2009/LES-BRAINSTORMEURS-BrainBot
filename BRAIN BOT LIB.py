3864420797a7762e203ae330ea4d22aa1046b9ea
Philippe2009 <45831602+Philippe2009@users.noreply.github.com>
2022-11-26 17:22:57 +0100

    Update BRAIN BOT LIB.py

--- BRAIN BOT LIB.py
+++ BRAIN BOT LIB.py
@@ -7,7 +7,6 @@ import math
 
 # Create your objects here.
 hub = MSHub()
-
 # Write your program here.
 hub.speaker.beep()
 # la variable des moteurs du robot
@@ -21,7 +20,7 @@ left_motor.set_degrees_counted(0)
 left_color_sensor = ColorSensor('A')
 right_color_sensor = ColorSensor('D')
 #le moteur du module du haut
-top_module_motor = Motor('E')
+motor_pair = MotorPair('E', 'F')
 # nous définissons que quand le robot s'arrête, il ne freine pas
 brain_bot.set_stop_action('coast')
 
@@ -100,28 +99,44 @@ def first_run():
 """
 Main: NE MARCHE PAS
 """
-move_until_straight(brain_bot,right_motor, 1550, 30)
 
-move_until_curve(brain_bot,right_motor,265 ,16,5)
-move_until_straight(brain_bot,right_motor, 600, 30)
+#motor_pair.move_tank(90, 'degrees', -90, -90)
+
+#move_until_straight(brain_bot,right_motor, 1550, 30)
+
+#move_until_curve(brain_bot,right_motor,265 ,16,5)
+#move_until_straight(brain_bot,right_motor, 600, 30)
+
+#move_until_straight(top_module_motor, 400, 30)
 #while right_color_sensor.get_reflected_light() <= 70:
  #brain_bot.start_tank(30,30)
 #while left_color_sensor.get_reflected_light() >= 45 and right_color_sensor.get_reflected_light() >= 45:
  #brain_bot.start_tank(50,50)
-'''
-brain_bot.stop()
-print("ligne noire")
-#move_until_curve(brain_bot,right_motor,1910 ,66,55)
-brain_bot.stop()
-'''
 
+#brain_bot.stop()
+#print("ligne noire")
+#move_until_curve(brain_bot,right_motor,1910 ,66,55)
+#brain_bot.stop()
+
+'''
 brain_bot.move(72,'cm',0,50)
 left_motor.run_for_degrees(-250, 50)
 for i in range(4):
     brain_bot.move(-10,'cm',0,100)
-    brain_bot.move(43,'cm',0,100)
+    wait_for_seconds(1)
+    brain_bot.move(20,'cm',0,100)
+
 left_motor.run_for_degrees(250, 100)
 brain_bot.move(-72,'cm',0,100)
+'''
 
+brain_bot.move(350,'degrees',0,50)
+right_motor.run_for_degrees(125, 30)
+brain_bot.move(900,'degrees',0,50)
+left_motor.run_for_degrees(-350, 30)
+for i in range(4):
+    brain_bot.move(350,'degrees',0,75)
+    wait_for_seconds(0.75)
+    brain_bot.move(-300,'degrees',0,25)
 
 
